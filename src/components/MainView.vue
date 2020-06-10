e05b619ab19e60ad3a2d0fcf53310d2fbfbaef98
disgraceful <noolic97@gmail.com>
2020-06-10 18:40:12 +0300

    Background image template

--- src/components/MainView.vue
+++ src/components/MainView.vue
@@ -6,4 +6,15 @@
 export default {};
 </script>
 
-<style lang="stylus" scoped></style>
+<style scoped>
+#bg {
+  /* background: url("../img/test-bg.jpg") no-repeat center center fixed; */
+  background-image: url("../img/test-bg.jpg");
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  background-attachment: fixed;
+  height: 100vh;
+  background-color: #464646;
+}
+</style>
