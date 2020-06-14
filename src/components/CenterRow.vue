cdbdfb42f75c21bb9f45a0ac72bde85fd4a3d0cc
disgraceful <noolic97@gmail.com>
2020-06-14 17:18:30 +0300

    Time format select buttons

--- src/components/CenterRow.vue
+++ src/components/CenterRow.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="center-row">
-    <div class="time">{{ formattedTime }}</div>
+    <div class="time-wrapper">
+      <!-- <div> -->
+      <div class="time">{{ formattedTime }}</div>
+      <div class="time-select">
+        <div>12</div>
+        <div>24</div>
+      </div>
+    </div>
+    <!-- </div> -->
     <div class="quote">Very profound quote</div>
   </div>
 </template>
@@ -27,9 +35,40 @@ export default {
   font-size: 1500%;
   font-weight: 500;
   letter-spacing: -5px;
+  margin-right: 20px;
 }
 
 .center-row .quote {
   font-size: 400%;
+}
+
+.time-wrapper {
+  display: inline-flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+
+.time-select {
+  align-content: stretch;
+}
+.time-select > div {
+  font-size: 28px;
+  margin: 10px;
+  padding: 7px;
+  opacity: 0;
+  transition: all 0.5s;
+  cursor: pointer;
+}
+
+.time:hover + .time-select > div,
+.time-select:hover > div {
+  opacity: 0.7;
+}
+
+.time-select > div:hover {
+  opacity: 1;
+  background-color: rgba(224, 224, 224, 0.5);
+  border-radius: 50%;
 }
 </style>
